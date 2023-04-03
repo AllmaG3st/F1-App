448531bf43c6ba4f6cead941a0e50d073c6b3c98
AlekoG <a.gevorkyan@sweeftdigital.com>
2023-04-03 16:26:39 +0400

    fix: add races screen to paramlist

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -4,6 +4,7 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 export type MainStackParamList = {
   Drivers: undefined;
   DriverInfo: {driverId: string};
+  DriverRaces: {driverId: string};
 };
 
 export type MainStackNavigationGenericProp<
